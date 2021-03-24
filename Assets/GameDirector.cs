a909228f0232f1b5381f9f87f5a09fa40ca4a86e
shinkyuhyun <81272310+shinkyuhyun@users.noreply.github.com>
2021-03-24 11:04:05 +0900

    30111-신규현(스와프카 완성)

---
+++ Assets/GameDirector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject Car;
+    GameObject flag;
+    GameObject distance;
+   
+    void Start()
+    {
+        this.Car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+    }
+
+    
+    void Update()
+    {
+        float length = this.flag.transform.position.x - this.Car.transform.position.x;
+        this.distance.GetComponent<Text>().text = "목표지점까지" + length.ToString("F2") + "m";
+    }
+}
